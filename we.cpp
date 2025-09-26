a0575bfdc6ae80d7e42fcf6d959e34a0f09f0bf4
karol <xxkitty2012@gmail.com>
2025-09-26 19:12:23 +0200

    Add files via upload

---
+++ we.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <Windows.h>
+using namespace std;
+
+int main()
+{
+    system("color 0e");
+    cout << "+---------+\n";
+    
+    for (int i = 1; i <= 10; i += 2) 
+    {    
+
+        int spacje = (9- i) / 2;
+
+        cout << "|";
+
+        for (int s = 0; s < spacje; s++) cout << " ";
+        for (int g = 0; g < i; g++) cout << "*";
+        for (int s = 0; s < spacje; s++) cout << " ";
+        cout <<"|\n";
+        Beep(200, 400);
+     
+        Sleep(600); 
+
+        cout << "+---------+\n";
+
+    }
+
+    return 0;
+}
